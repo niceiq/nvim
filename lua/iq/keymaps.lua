6cc5ddce6aeeeac06d8694c2b34195406c935eab
niceiq <98485752+niceiq@users.noreply.github.com>
2023-03-28 16:29:11 -0400

    Configure telescope

--- lua/iq/keymaps.lua
+++ lua/iq/keymaps.lua
@@ -55,3 +55,9 @@ keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
 keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
 keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
 keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
+
+-- Telescope --
+keymap("n", "<leader>ff", ":Telescope find_files<cr>", opts)
+keymap("n", "<leader>ft", ":Telescope live_grep<cr>", opts)
+keymap("n", "<leader>fp", ":Telescope projects<cr>", opts)
+keymap("n", "<leader>fb", ":Telescope buffers<cr>", opts)
