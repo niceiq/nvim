9d6dfa120608f3978968a66cbccdd6aaa9b6d127
niceiq <98485752+niceiq@users.noreply.github.com>
2023-03-28 17:07:27 -0400

    Configure cmp integration for nvim-autopairs

--- lua/iq/autopairs.lua
+++ lua/iq/autopairs.lua
@@ -21,3 +21,11 @@ npairs.setup {
       highlight_grey='Comment'
     },
 }
+
+local cmp_autopairs = require "nvim-autopairs.completion.cmp"
+local cmp_status_ok, cmp = pcall(require, "cmp")
+if not cmp_status_ok then
+  return
+end
+cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done {map_char = { tex  = ""} })
+
