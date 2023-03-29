9dc11182b29c648076ccf03cb2c89370d4a5fe20
niceiq <98485752+niceiq@users.noreply.github.com>
2023-03-28 22:45:03 -0400

    Configure toggleterm, sast commit for core config
    I'm done with my main config, I got all the necessay plugins I wanted.
    Now is the time to play around and really find out Neovim's true power.

--- lua/iq/toggleterm.lua
+++ lua/iq/toggleterm.lua
@@ -26,3 +26,42 @@ toggleterm.setup ({
     end
   },
 })
+
+function _G.set_terminal_keymaps()
+  local opts = {buffer = 0}
+  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+  -- vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', '<m-h>', [[<Cmd>wincmd h<CR>]], opts)
+  vim.keymap.set('t', '<m-j>', [[<Cmd>wincmd j<CR>]], opts)
+  vim.keymap.set('t', '<m-k>', [[<Cmd>wincmd k<CR>]], opts)
+  vim.keymap.set('t', '<m-l>', [[<Cmd>wincmd l<CR>]], opts)
+  vim.keymap.set('t', '<m-w>', [[<C-\><C-n><C-w>]], opts)
+end
+
+-- if you only want these mappings for toggle term use term://*toggleterm#* instead
+vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+
+-- Fast Terminal Apps
+
+local Terminal = require("toggleterm.terminal").Terminal
+
+local lazygit = Terminal:new({cmd = "lazygit", hidden = true})
+function _LAZYGIT_TOGGLE()
+  lazygit:toggle()
+end
+
+local node = Terminal:new({cmd = "node", hidden = true})
+function _NODE_TOGGLE()
+  node:toggle()
+end
+
+local htop = Terminal:new({cmd = "htop", hidden = true})
+function _HTOP_TOGGLE()
+  htop:toggle()
+end
+
+local python = Terminal:new({cmd = "python", hidden = true})
+function _PYTHON_TOGGLE()
+  python:toggle()
+end
+
