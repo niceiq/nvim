2197a0e73e4f85b762f2bf70a43b16d3356c5b99
niceiq <98485752+niceiq@users.noreply.github.com>
2023-03-28 20:19:30 -0400

    Configure Toggleterm

--- lua/iq/toggleterm.lua
+++ lua/iq/toggleterm.lua
@@ -2,3 +2,27 @@ local status_ok, toggleterm = pcall(require, "toggleterm")
 if not status_ok then
 	return
 end
+
+toggleterm.setup ({
+   -- size can be a number or function which is passed the current terminal
+  size = function(term)
+    if term.direction == "horizontal" then
+      return 10 
+    elseif term.direction == "vertical" then
+      return vim.o.columns * 0.4
+    end
+  end,
+  open_mapping = [[<m-\>]],
+  autochdir = true, -- when neovim changes it current directory the terminal will change it's own when next it's opened
+  shade_terminals = true, -- NOTE: this option takes priority over highlights specified so if you specify Normal highlights you should set this to false
+  shading_factor = 2, -- the percentage by which to lighten terminal background, default: -30 (gets multiplied by -3 if background is light)
+  direction = 'horizontal',
+  auto_scroll = true, -- automatically scroll to the bottom on terminal output
+  -- This field is only relevant if direction is set to 'float'
+  winbar = {
+    enabled = false,
+    name_formatter = function(term) --  term: Terminal
+      return term.name
+    end
+  },
+})
